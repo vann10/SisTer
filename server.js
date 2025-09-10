4efa8631776196f9e854ea16c616d7f58801a96c
vann10 <irvnurikh@gmail.com>
2025-09-11 01:25:00 +0700

    adjust dikit

--- server.js
+++ server.js
@@ -3,21 +3,19 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware untuk mengizinkan Cross-Origin Resource Sharing (CORS)
-// Ini penting agar halaman web kita bisa mengakses API ini
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-// Endpoint untuk mengunduh file berdasarkan nama
+// Endpoint untuk mengunduh file
 app.get('/download/:filename', (req, res) => {
   const { filename } = req.params;
   const filePath = path.join(__dirname, 'public', filename);
 
   console.log(`Mencoba mengirim file: ${filePath}`);
 
-  // Mengirim file ke user untuk diunduh
+  // Mengirim file ke user
   res.download(filePath, (err) => {
     if (err) {
       console.error("Error mengirim file:", err);
